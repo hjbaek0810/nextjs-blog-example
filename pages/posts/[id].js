2a7ee66ec52b645733f57bd87367705ae1cf2e85
hjbaek0810 <doolgi0810@naver.com>
2023-04-06 22:41:38 +0900

    Feat: blog ui update and add mdx

---
+++ pages/posts/[id].js
@@ -0,0 +1,59 @@
+import utilStyles from '../../styles/utils.module.css';
+import Layout from '../../components/Layout';
+import Head from 'next/head';
+import { getAllPostIds, getPostData } from '../../lib/posts';
+import Date from '../../components/Date';
+import CodeBlock from '../../components/CodeBlock';
+
+import { MDXRemote } from 'next-mdx-remote';
+
+export async function getStaticPaths() {
+	const paths = getAllPostIds();
+	return {
+		paths,
+		fallback: false
+	};
+}
+
+export async function getStaticProps({ params }) {
+	const postData = await getPostData(params.id);
+	return {
+		props: {
+			postData
+		}
+	};
+}
+
+const Button = ({ children }) => {
+	return (
+		<button
+			className="bg-black dark:bg-white text-teal-200 dark:text-teal-700 text-lg rounded-lg px-5"
+			onClick={() => alert(`thanks to ${children}`)}
+		>
+			{children}
+		</button>
+	);
+};
+const components = { Button, CodeBlock };
+
+export default function Post({ postData }) {
+	return (
+		<>
+			<Layout>
+				<Head>
+					<title>{postData.title}</title>
+				</Head>
+				<article>
+					<h1 className={utilStyles.headingXl}>{postData.title}</h1>
+					<div className={utilStyles.lightText}>
+						<Date dateString={postData.date} />
+					</div>
+					{postData.contentHtml && (
+						<div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+					)}
+					{postData.mdxSource && <MDXRemote {...postData.mdxSource} components={components} />}
+				</article>
+			</Layout>
+		</>
+	);
+}
---
+++ posts/test1.md
@@ -0,0 +1,10 @@
+---
+title: 'test1'
+date: '2023-04-05'
+---
+
+test1
+
+```
+const hi = 'hello'
+```
